e85423611997a389b8bf22905ba2bfc9bc26f9d4
woytu <admin@woytu.com>
2019-02-14 22:26:52 +0800

    Update default_footer.php

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -2,7 +2,7 @@
     <div class="footer">
         © 2017-
         <?php echo date("Y")?>
-        <a href="https://woytu.com" target="_blank">woytu</a>
+        <a href="https://github.com/woytu/DirectoryLister" target="_blank">woytu</a>
         &nbsp
         <a href="https://github.com/woytu" target="_blank">GitHub</a>
         . All rights reserved. &nbsp
