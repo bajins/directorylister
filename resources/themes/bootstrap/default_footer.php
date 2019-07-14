98e471e50bf3a21639708372206f7586ace9b27d
woytu <admin@woytu.com>
2019-07-14 13:18:20 +0800

    更新底部信息

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,10 +1,12 @@
 <!-- 本文件放置页面底部内容 -->
-© 2017-
+<span>© 2017-</span>
 <?php echo date("Y") ?>
+<span>&nbsp;</span>
 <a href="https://github.com/woytu/DirectoryLister" target="_blank">woytu</a>
-&nbsp
+<span>&nbsp;</span>
 <a href="https://github.com/woytu" target="_blank">GitHub</a>
-. All rights reserved. &nbsp
+<span>. All rights reserved.</span>
+<span>&nbsp;</span>
 <a href="http://tongji.baidu.com/web/welcome/ico?s=7ac7cdb0162597bd65c904b23b4fe505">
     <img src="https://tongji.baidu.com/sc-web/image/icon/31.gif?__v=1547788763675" />
 </a>
